535d81f7e966625214d3bbb18fe65bd04040000c
paularosero <pao-0114@hotmail.com>
2023-05-17 22:17:52 -0400

    cru d

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,11 +1,10 @@
 class ClientsController < ApplicationController
-
+  before_action :set_client, only: %I[show edit update destroy]
   def index
     @clients = Client.all
   end
 
   def show
-    @client = Client.find(params[:id])
   end
 
   def new
@@ -14,27 +13,34 @@ class ClientsController < ApplicationController
 
   def create
     @client = Client.new(client_params)
-    @client.save
-    redirect_to client_path(@client)
+    if @client.save
+      redirect_to client_path(@client), notice: 'The client was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
-    @client = Client.find(params[:id])
   end
 
   def update
-    @client = Client.find(params[:id])
-    @client.update(client_params)
-    redirect_to client_path(@client)
+    if @client.update(client_params)
+      redirect_to client_path(@client), notice: 'The client was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @client = Client.find(params[:id])
     @client.destroy
     redirect_to clients_path, status: :see_other
   end
 
   private
+
+  def set_client
+    @client = Client.find(params[:id])
+  end
 
   def client_params
     params.require(:client).permit(:first_name, :last_name)
