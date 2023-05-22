ee94da10806eae94860b3253508c5344f4fcf583
paularosero <pao-0114@hotmail.com>
2023-05-22 02:09:11 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  root to: 'clients#index'
   resources :clients do
     resources :cars, only: %i[new create]
   end
